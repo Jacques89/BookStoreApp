c687e769eaa64562d877bf3e880cdbe7bfdf8e30
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-11-12 19:46:56 +0100

    added flatlist to home page and books now displaying

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react'
-import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import { 
+  View, 
+  Text, 
+  SafeAreaView, 
+  TouchableOpacity, 
+  Image, 
+  ScrollView, 
+  FlatList 
+} from 'react-native'
 
 import { COLORS, FONTS, SIZES, icons, images } from '../constants'
+
+const LineDivider = () => {
+  return(
+    <View style={{ width: 1, paddingVertical: 18}}>
+      <View style={{ flex: 1, borderLeftColor: COLORS.lightGray, borderLeftWidth: 1 }}>
+
+      </View>
+    </View>
+  )
+}
 
 const Home = () => {
   const profileData = {
@@ -9,7 +27,105 @@ const Home = () => {
     point: 200
   }
 
+  const bookOtherWordsForHome = {
+    id: 1,
+    bookName: 'Other Words For Home',
+    bookCover: images.otherWordsForHome,
+    rating: 4.5,
+    language: 'Eng',
+    pageNo: 341,
+    author: 'Jasmine Warga',
+    genre: [
+      'Romance', 'Adventure', 'Drama'
+    ],
+    read: '12k',
+    description: `Jude never thought she'd be leaving her beloved older brother and 
+    father behind, all the way across the ocean in Syria. But when things in her 
+    hometown start becoming volatile, Jude and her mother are sent to live in 
+    Cincinnati with relatives. At first, everything in America seems too fast 
+    and too loud. The American movies that Jude has always loved haven't quite prepared 
+    her for starting school in the US and her new label of 'Middle Eastern', an 
+    identity she's never known before. But this life also brings unexpected surprises, 
+    There are new friends, a whole new family, and a school musical that 
+    Jude might just try out for. Maybe America, too, is a place where Jude can be 
+    seen as she really is`,
+    backgroundColor: 'rgba(247, 239, 219, 0.9',
+    navTintColor: '#000'
+  }
+
+  const bookTheMetropolis = {
+    id: 2,
+    bookName: 'The Metropolis',
+    bookCover: images.theMetropolist,
+    rating: 4.1,
+    language: 'Eng',
+    pageNo: 272,
+    author: 'Seith Fried',
+    genre: [
+      'Adventure', 'Drama'
+    ],
+    read: '13k',
+    description: `In Metropolis, the gleaming city of tomorrow, the dream of the
+    great American city has been achieved. But all that is about to change, unless
+    a neurotic, rule-following bureaucrat and an irreverent, freewheeling
+    artificial intelligence can save the city from a mysterious terrorist plot that
+    threatens its very existence. Henry Thompson has dedicated his life to
+    improving America's infrastructure as a proud employee of the United States
+    Municipal survey. So when the agency comes under attack, he dutifully accepts
+    his unexpected mission to visit Metropolis looking for answers. But his plans
+    to investigate quietly, quickly, and carefully are interrupted by his new
+    parner: a day-drinking, know-it-all named OWEN, who also turns out to be the
+    projected embodiment of the agency's supercomputer. Soon, Henry and OWEN are
+    fighting to save not only their own lives and those of the city's millions of
+    inhabitants, but also the soul of Metropolis. The Metropolis is a thrilling,
+    funny and touching adventure story, a tour-de-force of imagination that
+    trenchantly explores our relationships to the cities around us and the 
+    technologies guiding us into the future.`,
+    backgroundColor: 'rgba(247, 239, 219, 0.9',
+    navTintColor: '#000'
+  }
+
+  const bookTheTinyDragon = {
+    id: 3,
+    bookName: 'The Tiny Dragon',
+    bookCover: images.theTinyDragon,
+    rating: 3.5,
+    language: 'Eng',
+    pageNo: 110,
+    author: 'Ana C. Bouvier',
+    genre: [
+      'Drama', 'Adventure', 'Romance'
+    ],
+    read: '13k',
+    description: `This sketchbook for kids is the perfect tool to improve your
+    drawing skills! Designed to encourage kids around the world to express their
+    uniqueness through drawing, sketching or doodling, this sketch book is filled
+    with 110 high quality blank pages for creations. Add some fun markers, crayons,
+    and art supplies and you have the perfect, easy gift for kids!`,
+    backgroundColor: 'rgba(119, 77, 143, 0.9',
+    navTintColor: '#FFF'
+  }
+
+  const booksData = [
+    {
+      ...bookOtherWordsForHome,
+      completion: '75%',
+      lastRead: '3d 5h',
+    },
+    {
+      ...bookTheMetropolis,
+      completion: '23%',
+      lastRead: '10d 5h',
+    },
+    {
+      ...bookTheTinyDragon,
+      completion: '10%',
+      lastRead: '1d 2h',
+    }
+  ]
+
   const [profile, setProfile] = useState(profileData)
+  const [books, setBooks] = useState(booksData)
 
   function renderHeader(profile) {
     return(
@@ -97,23 +213,169 @@ const Home = () => {
             onPress={() => {
               console.log('Claim')
             }}>
-              <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center',
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center',
               justifyContent: 'center'}}>
-                <Image 
-                  source={icons.claim_icon}
-                  resizeMode='contain'
-                  style={{ 
-                    width: 30,
-                    height: 30
-                  }}
-                />
-                <Text style={{ marginLeft: SIZES.base, ...FONTS.body3, 
+              <Image 
+                source={icons.claim_icon}
+                resizeMode='contain'
+                style={{ 
+                  width: 30,
+                  height: 30
+                }}
+              />
+              <Text style={{ marginLeft: SIZES.base, ...FONTS.body4, 
                 color: COLORS.white }}>
-                  Claim!
-                </Text>
-              </View>
-            </TouchableOpacity>
+                Claim!
+              </Text>
+            </View>
+          </TouchableOpacity>
+          
+          {/* Divider */}
+          <LineDivider />
 
+          {/* Get Point */}
+          <TouchableOpacity 
+            style={{ flex: 1 }}
+            onPress={() => {
+              console.log('Get point')
+            }}
+          >
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <Image 
+                source={icons.point_icon}
+                resizeMode='contain'
+                style={{
+                  width: 30,
+                  height: 30
+                }}
+              />
+              <Text 
+                style={{ 
+                  marginLeft: SIZES.base, 
+                  ...FONTS.body4, 
+                  color: COLORS.white
+                }}
+              >
+                Get Point
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          {/* Divider */}
+          <LineDivider />
+          
+          {/* My Card */}
+          <TouchableOpacity
+            style={{ flex: 1 }}
+            onPress={() => {
+              console.log('My Card')
+            }}
+          >
+            <View 
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <Image 
+                source={icons.card_icon}
+                resizeMode='contain'
+                style={{
+                  width: 30,
+                  height: 30
+                }}
+                onPress={() => {
+                  console.log()
+                }}
+              />
+              <Text 
+                style={{
+                  marginLeft: SIZES.base,
+                  ...FONTS.body4, 
+                  color: COLORS.white
+                }}
+              >My Card
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
+  function renderBookSection(booksData) {
+
+    const renderItem = ({item, index}) => {
+      return(
+        <TouchableOpacity 
+          style={{
+            flex: 1,
+            marginLeft: index == 0 ? SIZES.padding: 0,
+            marginRight: SIZES.radius
+          }}
+          onPress={() => console.log('My Book')}
+        >
+          {/* Book Cover */}
+          <Image 
+            source={item.bookCover}
+            resizeMode='cover'
+            style={{
+              width: 180,
+              height: 250,
+              borderRadius: 20
+            }}
+          />
+          {/* Book Info */}
+        </TouchableOpacity>
+      )
+    }
+
+    return(
+      <View style={{ flex: 1 }}>
+        {/* Header */}
+        <View 
+          style={{ 
+            paddingHorizontal: SIZES.padding, 
+            flexDirection: 'row',
+            justifyContent: 'space-between' 
+          }}
+        >
+          <Text style={{ ...FONTS.h2, color: COLORS.white }}>My Books</Text>
+
+          <TouchableOpacity
+            onPress={() => console.log('See More')}
+          >
+            <Text 
+              style={{ 
+                ...FONTS.body3, 
+                color: COLORS.lightGray,
+                alignSelf: 'flex-start',
+                textDecorationLine: 'underline' 
+              }}
+            >
+              See More
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Books */}
+        <View style={{ flex: 1, marginTop: SIZES.padding }}>
+          <FlatList 
+            data={booksData}
+            renderItem={renderItem}
+            keyExtractor={item => `${item.id}`}
+            horizontal 
+            showsHorizontalScrollIndicator={false}
+          />
         </View>
       </View>
     )
@@ -126,6 +388,16 @@ const Home = () => {
         {renderHeader(profile)}
         {renderButtonSection()}
       </View>
+
+      {/* Body Section */}
+      <ScrollView style={{ marginTop: SIZES.radius }}>
+        {/* Books Section */}
+        <View>
+          {renderBookSection(booksData)}
+        </View>
+        {/* Categories Section */}
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
